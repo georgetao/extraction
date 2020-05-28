31d439dbd40236ef3ec9438dfe76aa125b33f2e4
rocassius <rocassius@berkeley.edu>
2020-05-28 14:17:04 -0700

    working with pretrained models

--- scripts/contextMapper.py
+++ scripts/contextMapper.py
@@ -4,53 +4,95 @@
 import sys
 import re
 import numpy as np
-from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize
 
 # Regexes
 HTML = r'</?\w+/?>|>|<'
+BR = r'</?br/?>'
+BRBR = BR+BR
+MARK = r'</?mark/?>|>|<'
 WHITE = r'\s+'
+HYPHENS = r'---+'
+YW = "you wrote:"
+
+EMAIL_TIME = "[0-9]?[0-9]:[0-9][0-9]\s[AP]M"
+EA = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+PH = r"(\d{0,2}[\s\.-]{0,3}\(?\d{0,3}\)?[\s\.-]{0,3}\d{3}[\s\.-]{0,3}\d{4})"
+
+BLACK = "[^A-Za-z0-9\s\?!,\.;:/-]+"
+
+
+
+# Strings
+BREAK = 'BREAK'
 FORWARD = 'Forwarded by'
 SPACE = ' '
+EMAIL = ' EMAILADDRESS '
+PHONE = ' PHONE '
 
-# helper functions
-# erase_html = lambda r: BeautifulSoup(r, 'html5lib').get_text().strip()
+# components
+email_components = [
+    'Date:',
+    'From:',
+    'To:',
+    'Subject:',
+    'Re:',
+    'Mime-Version:',
+    'Content-Type:',
+    'Content-Transfer-Encoding:',
+    '-From:',
+    '-To:',
+    '-cc:',
+    '-bcc:',
+    '-Folder:',
+    '-Origin:',
+    '-FileName:'
+]
 
-def erase_html(text):
+def make_regex(lst):
+    return '|'.join(lst)
+
+break_regex = make_regex([BRBR, HYPHENS, YW])
+comp_regex = make_regex(email_components+[EMAIL_TIME])
+
+def clean(text):
+    text = re.sub(break_regex, BREAK, text)
     text = re.sub(HTML, SPACE, text)
-    text = re.sub(WHITE, SPACE, text)
+    text = re.sub(BLACK, SPACE, text)
     text = text.strip()
     return text
 
-# def clean(raw):
-#     # tokenize into sentences
-#     sentences = sent_tokenize(erase_html(raw))
-#     return sentences
+def clean_info(text):
+    text = re.sub(EA, EMAIL, text)
+    text = re.sub(PH, PHONE, text)
+    text = re.sub(WHITE, SPACE, text)
+    # text = re.sub(BLACK, SPACE, text)
+    text = text.strip()
+    return text
 
 for line in sys.stdin:
-    # load
-    message, task = line.split('\t')
-    
-    # clean
-    task, message = erase_html(task), erase_html(message)
-    sents = sent_tokenize(message)
-    
-    # identify task sentence
-    is_task = [bool(re.search(task, s)) for s in sents]
-    i = np.argmax([bool(re.search(task, s)) for s in sents])
-    print(is_task)
-    ts = sents[i]
-    if i > 0:
-        cs = sents[i-1]
-    else:
-        cs = ''
-    
-    # print(f'{cs}\t{ts}')
-    print(50*'=')
-    print('TASK', task)
-    print('CCCC', cs)
-    print('TSTS', ts)
-    print('####', message)
-
-    
    
+    try:
+        # load
+        message, task = line.split('\t')
+        # clean
+        task, message = clean(task), clean(message)    
+        sents = sum([sent_tokenize(m) for m in message.split(BREAK)], [])
+
+        # identify entire task sentence
+        i = np.argmax([bool(re.search(task, s)) for s in sents])
+        ts = sents[i]
+
+        # identify context
+        if i > 0 and not re.search(comp_regex, sents[i-1]):
+            cs = sents[i-1]
+            context_sents = [s for s in sents[:i-1] if not re.search(comp_regex, s)]
+            context = '. '.join(context_sents)
+        else:
+            cs, context = '',''
+
+        # print
+        print(f'{task}\t{clean_info(context)}\t{clean_info(cs)}\t{clean_info(ts)}')
+        
+    except:
+        continue
