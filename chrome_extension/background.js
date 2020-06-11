ae8ca489b6a7048e2056dd71d68ea711382c7331
Percival Chen <percivalchen@berkeley.edu>
2020-06-10 21:48:05 -0700

    added button that resets the page color of google

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -2,6 +2,9 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log('The color is green.');
     });
+    chrome.storage.sync.set({color2: '#ffffff'}, function() {
+      console.log('The color is white.');
+    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,13 +1,27 @@
 let changeColor = document.getElementById('changeColor');
-let changeColor2 = document.getElementById('changeColor');
+let reset = document.getElementById('reset');
 
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 });
 
+chrome.storage.sync.get('color2', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value2', data.color);
+});
+
 changeColor.onclick = function(element) {
     let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    	chrome.tabs.executeScript(
+        	tabs[0].id,
+        	{code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
+reset.onclick = function(element) {
+	let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     	chrome.tabs.executeScript(
         	tabs[0].id,
