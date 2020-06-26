b6cb9a096f0a3998a98a55246b39b78247986547
Percival Chen <percivalchen@berkeley.edu>
2020-06-25 18:17:42 -0700

    added bundle

--- chrome_extension/extension_bundle/popup.js
+++ chrome_extension/extension_bundle/popup.js
@@ -1,17 +1,21 @@
-function myFunction() {
+function summarize() {
 	// alert("sending request to cloud fn");
 
-	var email_text = document.getElementById("myTextArea").value;
+	var email_text = document.getElementById("myTextArea").value
 
 	function status(response) {
 	  if (response.status >= 200 && response.status < 300) {
+	  	alert('good response')
 	    return Promise.resolve(response)
 	  } else {
+	  	alert('bad response')
 	    return Promise.reject(new Error(response.statusText))
 	  }
 	}
 
 	var cloud_fn_url = 'https://us-central1-sigma-smile-251401.cloudfunctions.net/classifier'
+
+	console.log(cloud_fn_url)
 	
 	fetch(cloud_fn_url, {
 	    method: 'post',
@@ -21,11 +25,20 @@ function myFunction() {
 	    },
 	    body: JSON.stringify({"message": email_text})
 	  })
-	  .then(response => response.text())
+	  .then(response => response.json())
 	  .then(function (data) {
-	    // alert("data request succeeded with json response");
-	    // alert(data);
-	    document.getElementById("answer").value = data;
+	    // alert(data)
+	    reqs = data["requests"]
+	    certs = data["certainty"]
+	    console.log(data["requests"])
+
+	    // alert(reqs)
+	    // alert(certs)
+		for (var i=0; i < reqs.length; i++) {
+			var sentence = reqs[i]
+			var certainty = certs[i]
+			document.getElementById("answer").value +=  '\r\n' + sentence + '\r\n' + '\r\n';
+		}
 	    return data;
 	  })
 	  .catch(function (error) {
@@ -34,4 +47,4 @@ function myFunction() {
 	}
 
 
-document.getElementById('clickMe').addEventListener('click', myFunction);
+document.getElementById('clickMe').addEventListener('click', summarize);
---
+++ chrome_extension/gcfunction_bundle/main.py
@@ -0,0 +1,56 @@
+# import sys
+# import os
+import numpy
+from google.cloud import storage
+sys.path.append(os.path.abspath('models'))
+import preprocess
+import nltk
+nltk.download('punkt')
+from nltk import sent_tokenize
+import fasttext
+
+def classify_text(request):
+    request_json = request.get_json(silent=True)
+    email_text = ""
+    if request.args and 'message' in request.args:
+        email_text = request.args.get('message')
+    elif request_json and 'message' in request_json:
+        email_text = request_json['message']
+    else:
+        return f'Something is wrong with request json. :('
+
+    def preprocess_text(text):
+        text = sent_tokenize(email)
+        out = []
+        for sentence in text:
+            if type(sentence) == str:
+                # clean text
+                clean = preprocess.clean(sentence)
+                # clean info
+                clean = preprocess.clean_info(clean)
+
+                out.append(clean)
+            else:
+                out.append("")
+        return out
+    
+    print("load fasttext model")
+    # email_text = preprocess_text(email_text)
+    ft_model = fasttext.load_model("models/best_ft_model.bin")
+    sentences = sent_tokenize(email_text)
+    res_dict = {"full_text": email_text}
+    reqs = []
+    certs = []
+    for i in range(len(sentences)):
+        prediction = ft_model.predict(sentences[i])
+        if "1" in prediction[0][0]:
+            reqs.append(sentences[i])
+            certs.append(str(round(prediction[1][0], 2)))
+        # res_dict[i] = {"sentence": sentences[i], 
+        #                 "label": prediction[0][0],
+        #                 "certainty": round(prediction[1][0], 2)
+        #                 }
+    
+    res_dict["requests"] = reqs
+    res_dict["certainty"] = certs
+    return res_dict
