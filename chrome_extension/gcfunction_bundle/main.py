ae00d8f305fae0f0c526ef05904d004291f991c4
Percival Chen <percivalchen@berkeley.edu>
2020-07-02 11:34:44 -0700

    working through preprocessing

--- chrome_extension/gcfunction_bundle/main.py
+++ chrome_extension/gcfunction_bundle/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import numpy
-from google.cloud import storage
+# from google.cloud import storage
 sys.path.append(os.path.abspath('models'))
 import preprocess
 import nltk
@@ -10,7 +10,9 @@ from nltk import sent_tokenize
 import fasttext
 
 def classify_text(request):
+    print("request",request)
     request_json = request.get_json(silent=True)
+    print("request_json", request_json)
     email_text = ""
     if request.args and 'message' in request.args:
         email_text = request.args.get('message')
@@ -20,8 +22,9 @@ def classify_text(request):
         return f'Something is wrong with request json. :('
 
     def preprocess_text(text):
-        text = sent_tokenize(email)
+        text = sent_tokenize(text)
         out = []
+        final_text = ""
         for sentence in text:
             if type(sentence) == str:
                 # clean text
@@ -30,14 +33,17 @@ def classify_text(request):
                 clean = preprocess.clean_info(clean)
 
                 out.append(clean)
+                final_text+= " "+clean
             else:
                 out.append("")
-        return out
+        return final_text
     
-    print("load fasttext model")
     email_text = preprocess_text(email_text)
+
+    print("load fasttext model")
     ft_model = fasttext.load_model("models/best_ft_model.bin")
-    sentences = sent_tokenize(email_text)
+    # sentences = sent_tokenize(email_text)
+    sentences = email_text
     res_dict = {"full_text": email_text}
     reqs = []
     certs = []
