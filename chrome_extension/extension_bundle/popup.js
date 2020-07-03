530cc5f5f363cd1c0aa1494ae8971a9fe972c5cb
George Tao <georgetao258@gmail.com>
2020-07-02 17:43:58 -0700

    finished initial pseudoprototype

--- chrome_extension/extension_bundle/popup.js
+++ chrome_extension/extension_bundle/popup.js
@@ -4,7 +4,7 @@
 // TODO: ADD RESULT TO TODO LIST.
 
 //stage emails to be summarized
-function read_emails(email_list) {
+function read_emails() {
 	console.log("read");
 }
 
@@ -13,9 +13,59 @@ function pseudo_summarize() {
 	console.log("summarize");
 }
 
-//should take in a list of strings.
+//should take in string
 function add_to_todo() {
-	console.log("addstolist")
+	
+	//ALL OF THIS IS JUST SETTING UP THE ENVIRONMENT
+	var row_div = document.createElement("div");
+	var col1_div = document.createElement("div");
+	var col2_div = document.createElement("div");
+	var dropdown_div = document.createElement("div");
+	var dropdown_menu_div = document.createElement("div");
+
+	row_div.appendChild(col1_div);
+	row_div.appendChild(col2_div);
+	col2_div.appendChild(dropdown_div);
+
+	row_div.className += "row";
+	col1_div.className += "col-10";
+	col2_div.className += "col-2";
+	dropdown_div.className += "dropdown";
+	dropdown_menu_div.className += "dropdown-menu"
+	dropdown_menu_div.setAttribute("aria-labelledby", "dropdownMenuButton");
+
+	var text = document.createTextNode("This is a task.");
+	col1_div.appendChild(text);
+
+	var button = document.createElement("button");
+	button.className += "btn btn-secondary dropdown-toggle";
+	button.setAttribute("type", "button");
+	button.setAttribute("id", "dropdownMenuButton");
+	button.setAttribute("data-toggle", "dropdown");
+	button.setAttribute("aria-has-popup", "true");
+	button.setAttribute("aria-expanded", "false");
+	dropdown_div.appendChild(button);
+	dropdown_div.appendChild(dropdown_menu_div);
+
+	var buttonImg = document.createElement("img");
+	buttonImg.src = "img/settings.png";
+	buttonImg.alt = "Settings";
+	button.appendChild(buttonImg);
+
+
+	//DROPDOWN LINKS
+	var options = ["Sender: George Tao", "Go to Original E-mail", "Mark as Read", "Mark as Spam"];
+	var links =["#", "#", "#", "#"];
+	for (var i=0; i<options.length; i++){
+		var option = document.createElement("a");
+		option.className = "dropdown-item";
+		option.setAttribute("href", links[i]);
+		option.appendChild(document.createTextNode(options[i]));
+		dropdown_menu_div.appendChild(option);
+	}
+	
+	var element = document.getElementById("checklist");
+	element.appendChild(row_div);
 }
 
 function start() {
