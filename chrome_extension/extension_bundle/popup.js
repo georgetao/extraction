8385f30b1a1e9cdffdb6cc6a9f97586231706124
George Tao <georgetao258@gmail.com>
2020-07-05 23:03:06 -0700

    incorporated classification model and bootstrap stylesheets

--- chrome_extension/extension_bundle/popup.js
+++ chrome_extension/extension_bundle/popup.js
@@ -14,64 +14,68 @@ function pseudo_summarize() {
 }
 
 //should take in string
-function add_to_todo() {
-	
-	//ALL OF THIS IS JUST SETTING UP THE ENVIRONMENT
-	var row_div = document.createElement("div");
-	var col1_div = document.createElement("div");
-	var col2_div = document.createElement("div");
-	var dropdown_div = document.createElement("div");
-	var dropdown_menu_div = document.createElement("div");
+function add_to_todo(results) {
+	for (i in results) {
+		//ALL OF THIS IS JUST SETTING UP THE ENVIRONMENT
+		var row_div = document.createElement("div");
+		var col1_div = document.createElement("div");
+		var col2_div = document.createElement("div");
+		var dropdown_div = document.createElement("div");
+		var dropdown_menu_div = document.createElement("div");
 
-	row_div.appendChild(col1_div);
-	row_div.appendChild(col2_div);
-	col2_div.appendChild(dropdown_div);
+		row_div.appendChild(col1_div);
+		row_div.appendChild(col2_div);
+		col2_div.appendChild(dropdown_div);
 
-	row_div.className += "row";
-	col1_div.className += "col-10";
-	col2_div.className += "col-2";
-	dropdown_div.className += "dropdown";
-	dropdown_menu_div.className += "dropdown-menu"
-	dropdown_menu_div.setAttribute("aria-labelledby", "dropdownMenuButton");
+		row_div.className += "row";
+		col1_div.className += "col-10";
+		col2_div.className += "col-2";
+		dropdown_div.className += "dropdown";
+		dropdown_menu_div.className += "dropdown-menu"
+		dropdown_menu_div.setAttribute("aria-labelledby", "dropdownMenuButton");
 
-	var text = document.createTextNode("This is a task.");
-	col1_div.appendChild(text);
+		//TEXT GOES HERE
+		var text = document.createTextNode(results[i]);
+		col1_div.appendChild(text);
 
-	var button = document.createElement("button");
-	button.className += "btn btn-secondary dropdown-toggle";
-	button.setAttribute("type", "button");
-	button.setAttribute("id", "dropdownMenuButton");
-	button.setAttribute("data-toggle", "dropdown");
-	button.setAttribute("aria-has-popup", "true");
-	button.setAttribute("aria-expanded", "false");
-	dropdown_div.appendChild(button);
-	dropdown_div.appendChild(dropdown_menu_div);
+		var button = document.createElement("button");
+		button.className += "btn btn-secondary dropdown-toggle";
+		button.setAttribute("type", "button");
+		button.setAttribute("id", "dropdownMenuButton");
+		button.setAttribute("data-toggle", "dropdown");
+		button.setAttribute("aria-has-popup", "true");
+		button.setAttribute("aria-expanded", "false");
+		dropdown_div.appendChild(button);
+		dropdown_div.appendChild(dropdown_menu_div);
 
-	var buttonImg = document.createElement("img");
-	buttonImg.src = "img/settings.png";
-	buttonImg.alt = "Settings";
-	button.appendChild(buttonImg);
+		var buttonImg = document.createElement("img");
+		buttonImg.src = "img/settings.png";
+		buttonImg.alt = "Settings";
+		button.appendChild(buttonImg);
 
 
-	//DROPDOWN LINKS
-	var options = ["Sender: George Tao", "Go to Original E-mail", "Mark as Read", "Mark as Spam"];
-	var links =["#", "#", "#", "#"];
-	for (var i=0; i<options.length; i++){
-		var option = document.createElement("a");
-		option.className = "dropdown-item";
-		option.setAttribute("href", links[i]);
-		option.appendChild(document.createTextNode(options[i]));
-		dropdown_menu_div.appendChild(option);
+		//DROPDOWN LINKS
+		var options = ["Sender: George Tao", "Go to Original E-mail", "Mark as Read", "Mark as Spam"];
+		var links =["#", "#", "#", "#"];
+		for (var i=0; i<options.length; i++){
+			var option = document.createElement("a");
+			option.className = "dropdown-item";
+			option.setAttribute("href", links[i]);
+			option.appendChild(document.createTextNode(options[i]));
+			dropdown_menu_div.appendChild(option);
+		}
+		
+		var element = document.getElementById("checklist");
+		element.appendChild(row_div);
 	}
-	
-	var element = document.getElementById("checklist");
-	element.appendChild(row_div);
 }
 
-function start() {
+function start () {
 	read_emails();
 	pseudo_summarize();
-	add_to_todo();
+
+	summarize();
+	// add_to_todo(results);
 }
 
 function summarize() {
@@ -93,7 +97,7 @@ function summarize() {
 
 	console.log(cloud_fn_url)
 	
-	fetch(cloud_fn_url, {
+	return fetch(cloud_fn_url, {
 	    method: 'post',
 	    headers: {
 	      'Accept': 'application/json',
@@ -107,22 +111,27 @@ function summarize() {
 	    reqs = data["requests"]
 	    certs = data["certainty"]
 	    console.log(data["requests"])
+	    var results = [];
+
 
 	    // alert(reqs)
 	    // alert(certs)
 		for (var i=0; i < reqs.length; i++) {
 			var sentence = reqs[i]
 			var certainty = certs[i]
-			document.getElementById("answer").value += sentence + '\r\n' + '\r\n';
+			// document.getElementById("answer").value += sentence + '\r\n' + '\r\n';
+
+			results.push(sentence);
 		}
-	    return data;
+	    return results;
+	  })
+	  .then(function(data) {
+	  	add_to_todo(data);
 	  })
 	  .catch(function (error) {
 	    alert(error);
 	  });
 	}
-
-temp_emails = ["help me with my problem.", "You are cool."]
 
 document.getElementById('clickMe').addEventListener('click', start);
 
