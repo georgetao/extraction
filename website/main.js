7e68652bc3d58c1feb0a1da9213038ed33d88e77
Percival Chen <percivalchen@berkeley.edu>
2020-07-16 22:37:25 -0700

    Added header to About page

--- website/main.js
+++ website/main.js
@@ -1,8 +1,9 @@
+// This function allows you to 
 function copy(that){
-var inp =document.createElement('input');
-document.body.appendChild(inp)
-inp.value =that.textContent
-inp.select();
-document.execCommand('copy',false);
-inp.remove();
-}
+	var inp =document.createElement('input');
+	document.body.appendChild(inp)
+	inp.value =that.textContent
+	inp.select();
+	document.execCommand('copy',false);
+	inp.remove();
+	}
