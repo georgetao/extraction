8ce505730dabecf2db51d030ba8d9b9d3f30e778
Percival Chen <percivalchen@berkeley.edu>
2020-07-16 21:01:30 -0700

    Finished formatting most of the About page

---
+++ website/main.js
@@ -0,0 +1,8 @@
+function copy(that){
+var inp =document.createElement('input');
+document.body.appendChild(inp)
+inp.value =that.textContent
+inp.select();
+document.execCommand('copy',false);
+inp.remove();
+}
