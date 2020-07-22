47ad491c2b2343bb167f733740be1805f339607d
George Tao <georgetao258@gmail.com>
2020-07-21 18:39:29 -0700

    changed no task sentence found

--- chrome_extension/extension_bundle/popup.js
+++ chrome_extension/extension_bundle/popup.js
@@ -19,79 +19,83 @@ function read_emails() {
 }
 
 function add_one_item_to_todo(result, id_counter) {
+	if (result == "No Task Sentence Found.") {
+		alert("No task sentence found.");
+		return;
+	}
 	//ALL OF THIS IS JUST SETTING UP THE ENVIRONMENT
-		var col_div = document.createElement("div");
-		var card_div = document.createElement("div");
-		var tab_head_div = document.createElement("div");
-		var tab_div = document.createElement("div");
-		var card_body_div = document.createElement("div");
+	var col_div = document.createElement("div");
+	var card_div = document.createElement("div");
+	var tab_head_div = document.createElement("div");
+	var tab_div = document.createElement("div");
+	var card_body_div = document.createElement("div");
 
-		col_div.appendChild(card_div);
-		card_div.appendChild(tab_head_div);
-		card_div.appendChild(tab_div);
-		tab_div.append(card_body_div);
+	col_div.appendChild(card_div);
+	card_div.appendChild(tab_head_div);
+	card_div.appendChild(tab_div);
+	tab_div.append(card_body_div);
 
-		col_div.className += "col-12";
-		card_div.className += "card card-checklist";
-		tab_head_div.setAttribute("role", "tab");
-		tab_head_div.className += "tab-checklist";
-		tab_div.className += "collapse";
+	col_div.className += "col-12";
+	card_div.className += "card card-checklist";
+	tab_head_div.setAttribute("role", "tab");
+	tab_head_div.className += "tab-checklist";
+	tab_div.className += "collapse";
 
 
-		tab_div.setAttribute("id", "item".concat(id_counter));
-		tab_div.setAttribute("data-parent", "#checklist");
-		card_body_div.className += "card-body extraction-dropdown-top-margin";
+	tab_div.setAttribute("id", "item".concat(id_counter));
+	tab_div.setAttribute("data-parent", "#checklist");
+	card_body_div.className += "card-body extraction-dropdown-top-margin";
 
-		//TEXT GOES HERE
-		var inputText = document.createTextNode(result);
-		var header3 = document.createElement("h3");
-		var itemText = document.createElement("a");
-		var tabButton = document.createElement("a");
-		
-		tab_head_div.appendChild(header3);
-		tab_head_div.appendChild(tabButton);
-		header3.appendChild(itemText);
-
-		header3.className += "mb-0";
-
-		itemText.className += "text-action";
-		itemText.setAttribute("contenteditable", "true");
-		itemText.setAttribute("name", "task");
-		itemText.appendChild(inputText);
-
-		tabButton.className += "fa fa-caret-down d-flex align-items-center extraction-dropdown-button";
-		tabButton.setAttribute("data-toggle", "collapse");
-		tabButton.setAttribute("data-target", "#item".concat(id_counter))
-
-		//DROPDOWN LINKS
-		var option0 = document.createElement("span");
-		option0.appendChild(document.createTextNode("Sender: EXTRACTION"));
-		card_body_div.appendChild(option0);
-
-		// var option1 = document.createElement("a");
-		// option1.appendChild(document.createTextNode("Go to Original E-mail"));
-		// card_body_div.appendChild(option1);
-
-		var option2 = document.createElement("a");
-		option2.appendChild(document.createTextNode("Remove"));
-		card_body_div.appendChild(option2);
-
-		option2.onclick = function() {
-			col_div.style.display = "none"; 
-		}
-
-		var option3 = document.createElement("a");
-		option3.className += "extraction-dropdown-top-margin";
-		option3.appendChild(document.createTextNode("Move to Top"));
-		card_body_div.appendChild(option3);
-
-		option3.onclick = function() {
-			var list = document.getElementById("checklist");
-			list.insertBefore(col_div, list.childNodes[0]);
-		}
+	//TEXT GOES HERE
+	var inputText = document.createTextNode(result);
+	var header3 = document.createElement("h3");
+	var itemText = document.createElement("a");
+	var tabButton = document.createElement("a");
 	
-		var element = document.getElementById("checklist");
-		element.appendChild(col_div);
+	tab_head_div.appendChild(header3);
+	tab_head_div.appendChild(tabButton);
+	header3.appendChild(itemText);
+
+	header3.className += "mb-0";
+
+	itemText.className += "text-action";
+	itemText.setAttribute("contenteditable", "true");
+	itemText.setAttribute("name", "task");
+	itemText.appendChild(inputText);
+
+	tabButton.className += "fa fa-caret-down d-flex align-items-center extraction-dropdown-button";
+	tabButton.setAttribute("data-toggle", "collapse");
+	tabButton.setAttribute("data-target", "#item".concat(id_counter))
+
+	//DROPDOWN LINKS
+	var option0 = document.createElement("span");
+	option0.appendChild(document.createTextNode("Sender: EXTRACTION"));
+	card_body_div.appendChild(option0);
+
+	// var option1 = document.createElement("a");
+	// option1.appendChild(document.createTextNode("Go to Original E-mail"));
+	// card_body_div.appendChild(option1);
+
+	var option2 = document.createElement("a");
+	option2.appendChild(document.createTextNode("Remove"));
+	card_body_div.appendChild(option2);
+
+	option2.onclick = function() {
+		col_div.style.display = "none"; 
+	}
+
+	var option3 = document.createElement("a");
+	option3.className += "extraction-dropdown-top-margin";
+	option3.appendChild(document.createTextNode("Move to Top"));
+	card_body_div.appendChild(option3);
+
+	option3.onclick = function() {
+		var list = document.getElementById("checklist");
+		list.insertBefore(col_div, list.childNodes[0]);
+	}
+
+	var element = document.getElementById("checklist");
+	element.appendChild(col_div);
 }
 
 var id_counter = 0;
